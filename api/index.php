66946ea748a5bbadd71cfdeed823890b5f4ff393
vanzendrato <irvanberkatvatmanzend@gmail.com>
2026-05-02 05:28:38 +0700

    test full laravel

--- api/index.php
+++ api/index.php
@@ -8,4 +8,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require __DIR__ . '/../bootstrap/app.php';
 
-echo "Bootstrap OK!";
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
